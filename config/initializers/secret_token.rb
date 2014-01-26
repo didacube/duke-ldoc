adc2ac7e1ba2234e5f3273a86e226e8da89d4e64
Ashley Qian <ashley.qian@duke.edu>
2014-01-26 15:29:43 -0500

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DukeLDOC::Application.config.secret_key_base = '9f3ad76eee304aab819675c3d09a1514b58ba1cc2207d550bd28976ebf1642fca2f9b943b6c5fa57ab382f38f71075ef68edc41d3ab8ecfc77db1fda7759ded5'
